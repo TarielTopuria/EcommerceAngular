bc77f88d22b087d1837d1e544b7343acd046647b
TatoTopuria <Tato.Topuria@space.ge>
2025-12-22 15:10:37 +0400

    added loader

--- ecommerce-angular/src/app/app-module.ts
+++ ecommerce-angular/src/app/app-module.ts
@@ -1,6 +1,6 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing-module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -13,6 +13,8 @@ import { ProductDetailsComponent } from './pages/product-details/product-details
 import { CartComponent } from './pages/cart/cart.component';
 import { AddProductComponent } from './pages/admin/add-product/add-product';
 import { NotFoundComponent } from './pages/not-found/not-found';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { LoaderInterceptor } from './core/interceptors/loader.interceptor';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ import { NotFoundComponent } from './pages/not-found/not-found';
     ProductDetailsComponent,
     CartComponent,
     AddProductComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +37,12 @@ import { NotFoundComponent } from './pages/not-found/not-found';
     ReactiveFormsModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [App]
 })
